d2d7b932d9879a7133c4e7044b805c928dcc9a15
Maxence <maxence.cornet@vis.io>
2016-11-30 17:41:56 +0100

    Update sample with Client sdk integration

--- 11.Server-to-Server-Communication/components/visio-sdk.js
+++ 11.Server-to-Server-Communication/components/visio-sdk.js
@@ -3,7 +3,9 @@ new Vue({
 
   data: () => {
     return {
-
+      user_id: '',
+      input: '',
+      contactInput: ''
     }
   },
 
@@ -35,6 +37,32 @@ new Vue({
           meeting_name: "Test Meeting"
         }
       }, "ClientSDK");
+      document.getElementById("ClientSDK").style.display = "block";
+    },
+
+    showChatModule: function() {
+      var self = this;
+      Visio.api('/users/search', 'POST', {email: self.input}, function (status, response) {
+        if (status == 200) {
+          console.log(response.user_id);
+          Visio.ui.chatModule({
+            userId: response.user_id
+          },'visio-chat-module');
+        }
+      });
+    },
+
+    addContact: function() {
+      var self = this;
+      Visio.api('/rooms', 'POST', {
+        email: self.contactInput,
+        firstname:"Alan",
+        lastname:"Smithee",
+        force_accept: true
+      },
+      function(status, response) {
+        console.log(response);
+      });
     }
 
   }
