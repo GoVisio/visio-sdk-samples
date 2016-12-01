bc4a5e9b8a2f8e59a1c0395ad2c1ee9a45d5d924
Maxence <maxence.cornet@vis.io>
2016-12-01 18:10:10 +0100

    Update le login avec la méthode loginWithJWT du sdk client

--- 11.Server-to-Server-Communication/components/login.js
+++ 11.Server-to-Server-Communication/components/login.js
@@ -11,16 +11,13 @@ new Vue({
 
   methods: {
     logUser: function () {
-      this.$http.post('http://localhost:3003/admin/users/login', {}, {
-        headers: {
-          Authorization: 'JWT '+this.JWT
-        },
-        credentials: true
-      }).then(function (user) {
-        var iframe = document.getElementById('visio_local');
-        iframe.src = iframe.src;
-      }).catch(function (err) {
-        this.errorMessage = 'Error while contacting the server';
+      Visio.lognWithJWT(this.JWT, function(status, response) {
+        if (status === 200) {
+          var iframe = document.getElementById('visio_local');
+          iframe.src = iframe.src;
+        }else {
+          this.errorMessage = response;
+        }
       });
     }
   }
