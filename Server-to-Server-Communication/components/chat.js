2f66ced4a5877241a2125153a8705c7e08ff759d
Maxence <maxence.cornet@vis.io>
2017-01-23 15:20:56 +0100

    Deleted logs

--- Server-to-Server-Communication/components/chat.js
+++ Server-to-Server-Communication/components/chat.js
@@ -14,11 +14,8 @@ new Vue({
       var self = this;
       Visio.api('/users/search', 'POST', {email: self.email}, function (status, response) {
         if (status == 200) {
-          console.log(response.user_id);
-          console.log(response);
           Visio.api('/rooms/'+response.room_id+'/messages','POST',{text: self.message}, function(status,responseMessages){
             Visio.api('/rooms/'+response.room_id+'/messages','GET',{}, function(status,msgs){
-              console.log(msgs);
               self.messages = msgs;
               self.message = '';
               self.email = '';
