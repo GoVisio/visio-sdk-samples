aedc73087f3fe45ca17cfae89d813cf7fdf07c57
Maxence <maxence.cornet@vis.io>
2017-03-31 14:04:20 +0200

    Add config.js file

--- Server-to-Server-Communication/backend/helpers.js
+++ Server-to-Server-Communication/backend/helpers.js
@@ -1,20 +1,17 @@
 var Q = require('Q');
 var jwt = require('jsonwebtoken');
 var moment = require('moment');
-
-const VISIO_APIKEY = 'B0ca[6Ec<!ZzH91idN7i-Q)E<-x>1Q';
-const VISIO_SECRET = 'BfMfpG)y0Rhyv)-b?AU89q=m38fgt8L{9IoBVo?z';
-const VISIO_JWT_ENCODING_SECRET = '4MW)c2]f{2qaFm^3f[C36]>7c5rV9!x-9zNNR{V>';
+var config = require('../config.js');
 
 exports.signJWTToken = function () {
   var deferred = Q.defer();
 
   const timestamp = (moment.unix(new Date()) + 86400000000) / 1000000;
   jwt.sign(JSON.stringify({
-    "apikey": VISIO_APIKEY,
-    "secret": VISIO_SECRET,
+    "apikey": config.visio_apikey,
+    "secret": config.visio_secret,
     "exp": timestamp
-  }), VISIO_JWT_ENCODING_SECRET, {algorithm: 'HS256'}, function (err, token) {
+  }), config.visio_jwt_encoding_secret, {algorithm: 'HS256'}, function (err, token) {
     if (err) {
       deferred.reject(err);
     } else {
---
+++ Server-to-Server-Communication/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.base_url = 'https://api.sandbox.vis.io/admin'
+config.visio_apikey = 'rR]Ra1ucKQhekA#zZE!3<^j,L<RCS6';
+config.visio_secret = 'DI?x#w0tvgp$xXoqexErb(X}]>)[oA0HUD$:X[dF';
+config.visio_jwt_encoding_secret = '4MW)c2]f{2qaFm^3f[C36]>7c5rV9!x-9zNNR{V>';
+
+module.exports = config;
