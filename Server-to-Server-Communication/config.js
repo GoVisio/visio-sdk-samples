3c6a4a1577aa36cb321ed92e6ab0a29c431122ea
Maxence Cornet <maxence.cornet@vis.io>
2017-03-31 17:21:02 +0200

    Update config.js
    Fix prod base_url

--- Server-to-Server-Communication/config.js
+++ Server-to-Server-Communication/config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.base_url = 'https://api.sandbox.vis.io/admin'
+config.base_url = 'https://api.vis.io/admin'
 config.visio_apikey = 'YOUR_API_KEY';
 config.visio_secret = 'YOUR_API_SECRET';
 config.visio_jwt_encoding_secret = 'YOUR_JWT_ENCODING_SECRET';
